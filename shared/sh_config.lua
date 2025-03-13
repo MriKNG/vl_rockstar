eb94c9554e7484022484fb06250fa7c1b32fd223
KNG <kng@playliberty.com>
2025-03-13 04:14:03 +0100

    Adjusted config file

---
+++ shared/sh_config.lua
@@ -0,0 +1,14 @@
+Config = {}
+
+--[Notification Config]--
+Config.ShowDuration = true
+Config.Position = 'top-right'
+
+--[Menu Config]--
+Config.RecordIcon = 'video'
+Config.SaveClipIcon = 'floppy-disk'
+Config.DeleteClipIcon = 'trash'
+Config.CloseIcon = 'circle-xmark'
+
+--[Command Config]--
+Config.Command = 'rockstar'
