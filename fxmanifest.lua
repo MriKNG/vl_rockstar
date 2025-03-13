bfd1726355e2a698d619694eb02858b870bfd713
KNG <kng@playliberty.com>
2025-03-13 04:14:38 +0100

    fxmanifest updates

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,20 +1,34 @@
 fx_version 'cerulean'
-name 'nad_rockstar'
-description 'Rockstar Editor script, made by Nad#1223.'
-author 'Nad#1223'
 game 'gta5'
+name 'vl_rockstar'
+description 'A resource utilizing ox_lib context menu for enabling Rockstar Editor functionalities'
+
+--author 'Nad#1223' <-- Original
+--repository 'https://github.com/Nads-Development/nad_rockstar' <-- Original
+
+author 'KNG'
+repository 'https://github.com/MriKNG/vl_rockstar'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    '@qb-core/shared/locale.lua',
+    'shared/*.lua',
+}
 
 client_scripts {
-    '@menuv/menuv.lua',
-    'config.lua',
-    'cl_rockstar.lua'
+    'client/*.lua'
 }
 
 server_scripts {
-    'config.lua',
-    'sv_rockstar.lua'
 }
 
 dependencies {
-    'menuv'
+    'ox_lib'
 }
+
+files {
+    'locales/en.json',
+    'locales/*.json'
+}
+
+lua54 'yes'
